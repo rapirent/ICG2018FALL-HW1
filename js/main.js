62e9ebb52c4aad5696b95f2af7682293ce0293e0
rapirent <rapirent@gmail.com>
2018-11-06 16:59:52 +0800

    add sem shading

--- js/main.js
+++ js/main.js
@@ -301,7 +301,7 @@ function setViewPort() {
   gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 }
-//TODO
+
 function updateMVMatrix(number) {
   mat4.perspective(pMatrix, 45, gl.viewportWidth / gl.viewportHeight, 0.1, 2000.0);
   mat4.identity(mvMatrix);
